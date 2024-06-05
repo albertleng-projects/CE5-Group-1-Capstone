b9307bb7efaee82534c7e8e3c8f93e09afa5a75b
Albert Leng <albertleng@hotmail.com>
2024-06-05 22:30:51 +0800

    Correct path to index.html in server.js and move index.html to public folder

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 const port = 3000;
 
 // Serve static files from the "public" directory
-app.use(express.static);
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + 'index.html');
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 app.listen(port, () => {
